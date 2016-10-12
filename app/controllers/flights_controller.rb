799b57b1f191db2dc487ece1628096b121b8f5b7
Bernard Kanyolo <bernard.kanyolo@andela.com>
2016-10-13 00:54:33 +0300

    ft(views): update search fields and button

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -2,5 +2,12 @@ class FlightsController < ApplicationController
   def index
     @airports = Airport.order(country: :asc,
       city: :asc, name: :asc, code: :asc)
+    if
+  end
+
+  private
+
+  def flight_params
+    params.require().permit()
   end
 end
