31e1485caee0c194756d60f102a51fa913fe97a5
Bernard Kanyolo <bernard.kanyolo@andela.com>
2016-11-03 11:44:56 +0300

    ch: refactor flights controller adn make it cleaner

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,7 +1,6 @@
 class FlightsController < ApplicationController
-  before_action :get_airports, only: [:home]
-
   def home
+    @airports = Airport.airports_by_geography
   end
 
   def index
@@ -10,15 +9,5 @@ class FlightsController < ApplicationController
     passengers = params[:passengers]
     date = params[:date]
     @search = FlightSearch.new(origin, destination, passengers, date)
-    respond_to do |format|
-      format.js {}
-    end
-  end
-
-  private
-
-  def get_airports
-    @airports = Airport.order(country: :asc,
-      city: :asc, name: :asc, code: :asc)
   end
 end
