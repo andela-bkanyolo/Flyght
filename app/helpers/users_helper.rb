4eab5c89fa6a707da72c5129527b684d5fa19fb0
Bernard Kanyolo <bernard.kanyolo@andela.com>
2016-11-04 12:37:06 +0300

    ch: remove unused puts declarations

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,5 @@
 module UsersHelper
   def booking_expired(booking)
-    p booking.flight.departure_date(booking.flight.departure)
-    p Time.now
     booking.flight.departure_date(booking.departure) < Time.now
   end
 end
